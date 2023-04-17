8057fc5c28b34e404c54d2f96fc92bb3b6227b43
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2023-04-17 23:01:37 +0200

    Improve edmus read support

--- src/edmus.r
+++ src/edmus.r
@@ -1,6 +1,8 @@
 library(readr)
 library(dplyr)
+library(lubridate)
 library(stringr)
+library(tidyr)
 
 source("src/common.r")
 
@@ -13,11 +15,20 @@ edmus_load <- function(path) {
 
 edmus_personal <- edmus_load("data/edmus-personal-220813_133309-DEN.txt") |>
     mutate(
-        wait_and_see = as.logical(wait_and_see)
-    )
+        wait_and_see = as.logical(wait_and_see),
+        across(c(
+            date_of_birth, first_exam, date_consent_form, created,
+            last_modified, last_info, last_clinical_assessment,
+            starts_with("irreversible_") & !ends_with("_unknown_date"),
+            ends_with("_date") & !ends_with("_unknown_date") & -unknown_decease_date
+        ), dmy)
+    ) |>
+    drop_na(patient_id) |>
+    select(-ms_onset)
 
 edmus_diagnosis <- edmus_load("data/edmus-diagnosis-220811_121631-DEP.txt") |>
     mutate(
+        across(ms_onset, dmy),
         disease_course = factor(case_match(
             disease_course,
             1:2 ~ "RR",
@@ -28,5 +39,14 @@ edmus_diagnosis <- edmus_load("data/edmus-diagnosis-220811_121631-DEP.txt") |>
         ))
     )
 
-edmus_clinical <- edmus_load("data/edmus-clinical-220811_122451-DEP.txt")
-edmus_episodes <- edmus_load("data/edmus-episodes-220811_121723-DEP.txt")
+edmus_clinical <- edmus_load("data/edmus-clinical-220811_122451-DEP.txt") |>
+    mutate(across(date, dmy))
+
+edmus_episodes <- edmus_load("data/edmus-episodes-220811_121723-DEP.txt") |>
+    mutate(across(date, dmy))
+
+edmus_studies <- edmus_load("data/edmus-study-230405_130411-DEN.txt")
+edmus_protocols <- edmus_load("data/edmus-protocol-230405_130423-DEN.txt")
+
+edmus_trt_dm <- edmus_load("data/edmus-trt_dm-230405_130357-DEN.txt") |>
+    mutate(across(ends_with("_date"), dmy))
